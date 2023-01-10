a3d711615fed7acd0ba6fd2af1a3b1e432f00f51
Chandrakant Pal <sanjay161097@gmail.com>
2023-01-11 01:20:44 +0530

    tailwind setup

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
 import packageJson from "./package.json" assert { type: "json" };
+import postcss from "rollup-plugin-postcss"
 
 export default [
   {
@@ -24,11 +25,22 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss({
+        config: {
+          path: './postcss.config.cjs',
+        },
+        extensions: ['.css'],
+        minimize: true,
+        inject: {
+          insertAt: 'top',
+        },
+      }),
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+    external: [/\.css$/]
   },
 ];
