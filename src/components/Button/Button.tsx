d2d14a9bbc2ebe918f9f05560562e4fca8e2fad5
Chandrakant Pal <sanjay161097@gmail.com>
2023-01-16 18:53:56 +0530

    button extends other props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,22 @@
 import * as React from "react";
 
-interface ButtonProps {
+interface ButtonProps
+  extends React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
   label: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ label }) => {
-  return <button className="px-4 py-2 text-center bg-red-500 outline-none rounded-lg text-white">{label}</button>;
+const Button: React.FC<ButtonProps> = ({ label, ...props }) => {
+  return (
+    <button
+      className="px-4 py-2 text-center bg-red-500 outline-none rounded-lg text-white"
+      {...props}
+    >
+      {label}
+    </button>
+  );
 };
 
 export default Button;
