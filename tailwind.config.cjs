306a2699deb54d648042fc9b5a2902240f988f68
Chandrakant Pal <sanjay161097@gmail.com>
2023-01-18 01:29:05 +0530

    colors extended

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#CC1313",
+        secondary: "#4C9D8A",
+        nevigate: "#EC7622",
+      },
+    },
   },
   plugins: [],
-}
+};
