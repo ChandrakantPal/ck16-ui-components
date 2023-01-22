b74c3cfd0832b553ea041f8224b7eff4baf638f5
Chandrakant Pal <sanjay161097@gmail.com>
2023-01-22 13:27:55 +0530

    button colors are now from custom colors

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -12,19 +12,19 @@ export interface ButtonProps
 
 const ButtonConfig = {
   text: {
-    primary: "text-[#CC1313]",
-    secondary: "text-[#4C9D8A]",
-    nevigate: "text-[#EC7622]",
+    primary: "text-primary",
+    secondary: "text-secondary",
+    nevigate: "text-nevigate",
   },
   outlined: {
-    primary: "border-[#CC1313] border text-[#CC1313]",
-    secondary: "border-[#4C9D8A] border text-[#4C9D8A]",
-    nevigate: "border-[#EC7622] border text-[#EC7622]",
+    primary: "border-primary border text-primary",
+    secondary: "border-secondary border text-secondary",
+    nevigate: "border-nevigate border text-nevigate",
   },
   contained: {
-    primary: "bg-[#CC1313] text-white",
-    secondary: "bg-[#4C9D8A] text-white",
-    nevigate: "bg-[#EC7622] text-white",
+    primary: "bg-primary text-white",
+    secondary: "bg-secondary text-white",
+    nevigate: "bg-nevigate text-white",
   },
 };
 
